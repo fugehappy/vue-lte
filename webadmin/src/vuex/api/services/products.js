54de88da8b96cc150fc01733acc2cdc4704a8b97
fugehappy <fugehappy@example.com>
2017-06-02 13:15:32 +0800

    port

--- webadmin/src/vuex/api/services/products.js
+++ webadmin/src/vuex/api/services/products.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 export default {
   getAll () {
-    return axios.get('http://localhost:3000/products')
+    return axios.get('http://localhost:9728/products')
       .then((response) => Promise.resolve(response))
       .catch((error) => Promise.reject(error))
   },
 
   getOne (request = {}) {
-    return axios.get('http://localhost:3000/products', request)
+    return axios.get('http://localhost:9728/products', request)
       .then((response) => Promise.resolve(response))
       .catch((error) => Promise.reject(error))
   }
